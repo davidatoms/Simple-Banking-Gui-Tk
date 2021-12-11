c889539de8f81d8237f4d909b976deba5702e777
DA <david_mi_adams@outlook.com>
2021-12-11 13:51:03 -0700

    banking app

--- banking-gui-app/banking-gui.py
+++ banking-gui-app/banking-gui.py
@@ -10,23 +10,21 @@ master.title('Banking App')
 # functions
 def finish_reg():
 	name = temp_name.get()
-	age = temp_age.get()
 	password = temp_password.get()
 	all_accounts = os.listdir()
 
-	if name == "" or age == "" or password == "":
+	if name == "" or password == "":
 		notif.config(fg="red",text="All fields are required *")
 		return
-	print("Good to go!")
 
 	for name_check in all_accounts:
 		if name == name_check:
 			notif.config(fg="red",text="Account already exists")
+			return
 		else:
-			new_file = open(name,"w")
+			new_file = open(name, "w")
 			new_file.write(name+'\n')
 			new_file.write(password+'\n')
-			new_file.write(age+'\n')
 			new_file.close()
 			notif.config(fg="green",text="Account has been created")
 
@@ -37,25 +35,24 @@ def register():
 
 	# vars
 	global temp_name
-	global temp_age
 	global temp_password
 	global notif
 	temp_name = StringVar()
-	temp_age = StringVar()
 	temp_password = StringVar()
+
 
 	# labels
 	Label(register_screen, text="Please enter your details below to register", font=('Calibri',12)).grid(row=0,sticky=N,pady=10)
 	Label(register_screen, text="Name", font=('Calibri',12)).grid(row=2,sticky=W)
-	Label(register_screen, text="Age", font=('Calibri',12)).grid(row=3,sticky=W)
 	Label(register_screen, text="Password", font=('Calibri',12)).grid(row=4,sticky=W)
 	notif = Label(register_screen, font=("Calibri",12))
 	notif.grid(row=6,sticky=N,pady=10)
 
+
 	# entries
 	Entry(register_screen,textvariable=temp_name).grid(row=2,column=0)
-	Entry(register_screen,textvariable=temp_age).grid(row=3,column=0)
 	Entry(register_screen,textvariable=temp_password,show="*").grid(row=4,column=0)
+
 
 	# buttons
 	Button(register_screen,text="Register",command = finish_reg, font=('Calibri',12)).grid(row=5,sticky=N,pady=10)
@@ -80,7 +77,4 @@ Button(master, text="Register", font=('Calibri',12),width=20,command=register).g
 Button(master, text="Login", font=('Calibri',12),width=20,command=login).grid(row=4,sticky=N,pady=10)
 
 master.mainloop()
-
-
-
 
